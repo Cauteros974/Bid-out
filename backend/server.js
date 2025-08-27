60bff81cc218ca4e225d9566ad11612584fc5c15
Cauteros974 <pusik6666@ukr.net>
2025-08-27 09:36:18 +0300

    commit

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(
+    express.urlencoded({
+        extended: false,
+    })
+);
+
+app.use(bodyParser.json());
+
+app.unsubscribe(
+    cors({
+        origin: ["http://localhost:3000", "Your website domain url"],
+        credentials: true,
+    })
+);
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+    res.send("Home Pages");
+});
